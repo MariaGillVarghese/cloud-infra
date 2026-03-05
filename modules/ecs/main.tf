1f6fa618276c9f636c9d237bc4e524fa753fea8a
MariaGillVarghese <mariagillvarghese@gmail.com>
2026-03-05 21:52:46 +0100

    Added ecr and ecs

---
+++ modules/ecs/main.tf
@@ -0,0 +1,65 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = var.cluster_name
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "${var.cluster_name}-sg"
+  description = "Security group for ECS service"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = var.container_port
+    to_port     = var.container_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = var.task_name
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.cpu
+  memory                   = var.memory
+
+  execution_role_arn = var.execution_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name  = var.container_name
+      image = var.container_image
+
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnet_ids
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_ecs_task_definition.task]
+}
